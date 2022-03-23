57bda54c270514e5aa2aec00e4343bad3f191596
AlexisEche <alexis.eche58@gmail.com>
2022-03-23 13:22:34 -0500

    high order functions: filter, map and reduce

--- comprenhensions/list_and_dics.py
+++ comprenhensions/list_and_dics.py
@@ -1,8 +1,14 @@
+from functools import reduce
+
 def main():
-  palindrome = lambda string : string == string[::-1]
+  my_list = [1,4,5,6,9,13,19,21]
+  my_list_even = list(filter(lambda x: (x%2 != 0) ,my_list))
+  my_list_square = list(map(lambda x: x**2 ,my_list))
+  my_list_reduce = reduce(lambda x,y : x*y ,my_list)
 
-  print(palindrome("Holi"))
-
+  print(my_list_even)
+  print(my_list_square)
+  print(my_list_reduce)
 
   
 if __name__ == '__main__':
